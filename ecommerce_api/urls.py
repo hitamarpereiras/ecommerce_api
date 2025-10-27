f1c1dbf25e26e1683883337eb8a986c61d0bbfd2
hitamarpereiras <hitamarsilvaoficial@gmail.com>
2025-10-27 08:59:40 -0300

    Autenticção adicionada

--- ecommerce_api/urls.py
+++ ecommerce_api/urls.py
@@ -1,4 +1,5 @@
 from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
@@ -12,5 +13,7 @@ route.register(r"products", productsviewset.ProductViews, basename='Products')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('token/', TokenObtainPairView.as_view()),
+    path('token', TokenRefreshView.as_view()),
     path('', include(route.urls))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- products/api/viewsets.py
+++ products/api/viewsets.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from products.api import serializers
 from products import models
 
 class ProductViews(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated, )
     serializer_class = serializers.ProductSerializers
     queryset = models.Product.objects.all()
