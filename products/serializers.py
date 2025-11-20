a500ffe6c48991924f6ca97b931ac1fca57bedf6
hitamarpereiras <hitamarsilvaoficial@gmail.com>
2025-11-20 08:55:00 -0300

    21º fix: corrigido erro em serializer

--- products/serializers.py
+++ products/serializers.py
@@ -1,21 +1,20 @@
 from rest_framework import serializers
 from .models import Produto
-
+    
 class ProdutoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Produto
         fields = '__all__'
+        read_only_fields = ["id", "img_url", "hora_criacao", "data_criacao"]
 
     def validate_nome(self, value):
-        if len(value) < 3:
+        if not value:
             raise serializers.ValidationError("O nome deve ter pelo menos 3 caracteres")
+        
         return value
     
-    def validate_nome(self, value):
+    def validate_preco(self, value):
         if value < 0:
             raise serializers.ValidationError("O preço não pode ser negativo")
-        
-        elif not value.isdigit():
-            raise serializers.ValidationError("Preço invalido")
         
         return value
