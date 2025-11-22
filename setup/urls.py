d25d317ed8840ba99cdae326eabd8dc0d00ee8d6
hitamarpereiras <hitamarsilvaoficial@gmail.com>
2025-11-22 09:42:49 -0300

    23º importação rest_framework em setup/views.py

--- setup/urls.py
+++ setup/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.http import JsonResponse
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,   # pega access + refresh (login)
+    TokenRefreshView,     # troca refresh por novo access (e opcionalmente novo refresh se ROTATE)
+    TokenVerifyView,      # verifica um token (útil em debug)
+)
 
 def home(request):
     return JsonResponse({'message': 'API Django no ar 🚀'})
@@ -9,4 +14,7 @@ urlpatterns = [
     path('', home),  # rota principal opcional
     path('admin/', admin.site.urls),
     path('produtos/', include('products.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
